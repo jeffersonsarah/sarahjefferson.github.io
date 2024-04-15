73c841ea6ec34e980ded0e782425020afc21c49f
jeffersonsarah <156980384+jeffersonsarah@users.noreply.github.com>
2024-04-14 22:01:37 -0500

    Create charity_prfile.js

---
+++ charity_prfile.js
@@ -0,0 +1,23 @@
+<!-- HTML for the Volunteer button -->
+    
+      <button id="volunteerButton">Volunteer Now</button>
+      <div id="contactForm" style="display: none;">
+        <form id="volunteerForm">
+          <label for="nameInput">Name:</label>
+          <input type="text" id="nameInput" required><br>
+          <label for="emailInput">Email:</label>
+          <input type="email" id="emailInput" required><br>
+          <label for="CharityInput">Charity:</label>
+          <input type="description" id="descriptionInput" required><br>
+          <input type="submit" value="Submit">
+          
+        </form>
+      </div>
+      <div id="interactiveElement">
+        <!-- Your additional interactive element here -->
+      </div>
+      <script src="charity_profile.js"></script>
+
+<!-- Additional interactive element -->
+<div id="additionalElement">
+</div>
